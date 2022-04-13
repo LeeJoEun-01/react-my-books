a9ac23260a75533dcb4c5f824a24b53131ea124b
LeeJoEun-01 <joeun1005@naver.com>
2022-04-13 16:40:09 +0900

    Create Project(1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './pages/Home';
+import Edit from './pages/Edit';
+import Detail from './pages/Detail';
+import Add from './pages/Add';
+import Signin from './pages/Signin';
+import NotFound from './pages/Notfound';
+import Error from './pages/Error';
+import { ErrorBoundary } from 'react-error-boundary';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/edit/:id" component={Edit} />
+        {/* 상세 페이지 */}
+        <Route exact path="/book/:id" component={Detail} />
+        <Route exact path="/add" component={Add} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/" component={Home} />
+        {/* 위의 경우가 아닌 경로로 갔을 때 Not Found 띄워주는 페이지 */}
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
